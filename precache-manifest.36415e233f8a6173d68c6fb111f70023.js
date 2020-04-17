3f93ecc2d8c492f161a8ca1e1a0f77b150859b89
Stanislav <stanislav.khomyn@skylum.com>
2020-04-17 16:58:51 -0700

    Updates

---
+++ precache-manifest.36415e233f8a6173d68c6fb111f70023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ad779ed1fb695686f123e1bfd58d7b",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "e3d6e0a1481709492845",
+    "url": "/react-todo-app/static/css/main.dd1e46a3.chunk.css"
+  },
+  {
+    "revision": "22534dfa7cb9e779fa21",
+    "url": "/react-todo-app/static/js/2.0d4fbae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.0d4fbae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d6e0a1481709492845",
+    "url": "/react-todo-app/static/js/main.b13b2fa4.chunk.js"
+  },
+  {
+    "revision": "1796198026348bea5667",
+    "url": "/react-todo-app/static/js/runtime-main.3af68235.js"
+  }
+]);
